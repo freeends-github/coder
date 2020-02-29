e976c11a3a92011a2dcc64be99d2143386b1b368
agnesa12 <agnesanuha4@gmail.com>
2020-02-29 22:27:40 +0100

    final update

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-    function progress() {
-      var elem = document.getElementById("html%");
-      var width = 1;
-      var id = setInterval(frame, 10);
-      function frame() {
-        if (width >= 100) {
-          clearInterval(id);
-        } else {
-          width++;
-          elem.style.width = width + '%';
-        }
-      }
-    }
+
+   
     
